11aadf190e3a837097dec5451c98b8c741f45144
Sychotix <sychotixdev@gmail.com>
2018-03-19 13:49:52 -0400

    Reorganized things

--- src/BuildYourOwnRoutineSettings.cs
+++ src/BuildYourOwnRoutineSettings.cs
@@ -3,6 +3,8 @@ using PoeHUD.Hud.Settings;
 using PoeHUD.Plugins;
 using System.Windows.Forms;
 using TreeRoutine.Routine.BuildYourOwnRoutine.Flask;
+using ImGuiVector2 = System.Numerics.Vector2;
+using ImGuiVector4 = System.Numerics.Vector4;
 
 namespace TreeRoutine.Routine.BuildYourOwnRoutine
 {
@@ -10,7 +12,10 @@ namespace TreeRoutine.Routine.BuildYourOwnRoutine
     {
         public BuildYourOwnRoutineSettings() : base()
         {
-
+            var centerPos = BasePlugin.API.GameController.Window.GetWindowRectangle().Center;
+            LastSettingSize = new ImGuiVector2(620, 376);
+            LastSettingPos = new ImGuiVector2(centerPos.X - LastSettingSize.X / 2, centerPos.Y - LastSettingSize.Y / 2);
+            ShowSettings = false;
         }
 
         public FlaskSetting[] FlaskSettings { get; internal set; } = new FlaskSetting[5]
@@ -25,5 +30,10 @@ namespace TreeRoutine.Routine.BuildYourOwnRoutine
         public RangeNode<int> TicksPerSecond { get; internal set; } = new RangeNode<int>(10, 1, 30);
 
         public LoadedProfile LoadedProfile { get; set; }
+
+        public ImGuiVector2 LastSettingPos { get; set; }
+        public ImGuiVector2 LastSettingSize { get; set; }
+
+        public ToggleNode ShowSettings { get; set; }
     }
 }
